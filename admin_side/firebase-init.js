e4016b80f502c2896f2eb45901c8d14507efded9
Lauofthesea <hz202300625@wmsu.edu.ph>
2025-11-17 00:37:11 +0800

    updated

--- admin_side/firebase-init.js
+++ admin_side/firebase-init.js
@@ -1,4 +1,3 @@
-// firebase-init.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
